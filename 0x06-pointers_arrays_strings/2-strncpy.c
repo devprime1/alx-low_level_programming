ed2efa2de17fdbc9e1d0dd1881493d4c6e0b4e57
chibuisi ukegbu <chibuisiukegbu@gmail.com>
2022-09-21 23:52:51 -0700

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,8 +12,12 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	for (n = 0; src[i] != '\0'; n++)
-		dest[i] = src[i];
-	dest[i] = '\0';
-	return (dest);
+	int i = 0, slen = 0;
+       while(src[i++])
+               slen++;
+       for (i = 0; src[i] && i < n; i++)
+	       dest[i] =src[i];
+       for (i = slen; i < n; i++)
+	       dest[i] ='\0';
+       return (dest);
 }
