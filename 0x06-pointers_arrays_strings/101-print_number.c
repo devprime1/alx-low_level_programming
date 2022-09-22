a3dc0b0442656d5af01db15874e4bebe68e1a101
chibuisi ukegbu <chibuisiukegbu@gmail.com>
2022-09-22 01:46:01 -0700

    Task 101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,14 +7,14 @@ void print_number(int n)
 {
 	unsigned int num = n;
 
-	if (0 > n)
+	if (n < 0)
 	{
 		_putchar('-');
 		num = -num;
 	}
 	if ((num / 10) > 0)
 	{
-		print_number(num /10);
+		print_number(num / 10);
 	}
 	_putchar((num % 10) + '0');
 }
