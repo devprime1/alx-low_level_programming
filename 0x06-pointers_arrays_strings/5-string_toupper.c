0a13b7081088556474a4ae1086f5ed75eee171cc
chibuisi ukegbu <chibuisiukegbu@gmail.com>
2022-09-22 01:00:03 -0700

    Task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,9 +12,11 @@ char *string_toupper(char *str)
 
 	while (str[i])
 	{
-		if (str[i] > 'a' && str[i] < 'z')
+		if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] -= 32;
+
 		i++;
 	}
+
 	return (str);
 }
