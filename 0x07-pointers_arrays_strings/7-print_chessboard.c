4cc791886b292bf1447123f074148de12df1f55d
chibuisi ukegbu <chibuisiukegbu@gmail.com>
2022-09-27 03:39:54 -0700

    Task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/* main: checks code
+ * print_chessboard - prints 2D array of chessboard
+ * @a: chessboard array
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int r, c;
+
+	for (r = 0; r < 8; r++)
+	{
+		for (c = 0; c < 8; c++)
+		{
+			_putchar(a[r][c]);
+		}
+		_putchar('\n');
+	}
+}
