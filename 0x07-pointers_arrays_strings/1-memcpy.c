e2252f1067ad2642e4008f98eeea2c2d85de8896
chibuisi ukegbu <chibuisiukegbu@gmail.com>
2022-09-27 01:19:06 -0700

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,13 +10,9 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int i;
-	char *src_a, *dest_b;
-
-	*src_a = (char *)src;
-	*dest_b = (char *)dest;
 	for (i = 0; i < n; ++i)
 	{
-		dest_b[i] = src_a[i];
+		dest[i] = src[i];
 	}
 	return (dest);
 }
