3da9d5002b6baf7309500332468537698e6f370b
chibuisi ukegbu <chibuisiukegbu@gmail.com>
2022-09-28 01:36:55 -0700

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+	int l = 0;
+
+	if (*s != NULL)
+	{
+		l++;
+		_strlen_recursion(++s);
+	}
+	else
+	{
+		return l;
+	}
+}
