f19c439acd4bc3a2f5ae61cc1e0e1aa6149bcbc0
chibuisi ukegbu <chibuisiukegbu@gmail.com>
2022-10-01 15:18:50 -0700

    Tasks

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints name of a program
+ * @argc - number of command line arguments
+ * @argv - name of the array
+ * Return: 0.
+ */
+
+int main(int argc, *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+	return (0);
+}
