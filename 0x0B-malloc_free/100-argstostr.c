bf89d3f79e5335232e568051016b6c9765ac5962
chibuisi ukegbu <chibuisiukegbu@gmail.com>
2022-10-03 07:03:29 -0700

    Task 100

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * argstostr - convert the parameters passed to the program to string
+  * @ac: the argument count
+  * @av: the argument vector
+  * Return: ...
+  */
+
+char *argstostr(int ac, char **av)
+{
+	int i = 0, j = 0, count = 0;
+	char *res, *new;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	while (i < ac)
+	{
+		new = av[i];
+		while (*new)
+		{
+			count++;
+			new++;
+		}
+		count++;
+		i++;
+	}
+	res = malloc(count + 1);
+	i = 0;
+	while (i < ac)
+	{
+		new = av[i];
+		while (*new)
+		{
+			res[j] = *new;
+			new++;
+			j++;
+		}
+		res[j] = '\n';
+		j++;
+		i++;
+	}
+	result[count + 1] = '\0';
+	return (res);
+}
